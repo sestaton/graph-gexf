705e50772bbd578587c128948e1a3ba97de17a15
franck cuny <franck@lumberjaph.net>
2011-05-21 16:57:31 +0200

    add method to test if position has been defined
    Signed-off-by: franck cuny <franck@lumberjaph.net>

--- lib/Graph/GEXF/Role/Viz/Position.pm
+++ lib/Graph/GEXF/Role/Viz/Position.pm
@@ -2,10 +2,15 @@ package Graph::GEXF::Role::Viz::Position;
 
 use Moose::Role;
 
+my $_has_position = 0;
+
 has [qw/x y z/] => (
-    is  => 'rw',
-    isa => 'Num',
+    is      => 'rw',
+    isa     => 'Num',
+    trigger => sub { $_has_position++ },
 );
+
+sub has_position { $_has_position }
 
 no Moose::Role;
 
