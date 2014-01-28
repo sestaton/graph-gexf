ecc991297c602b5844f2ffae8d12baf5f37c8c0e
sestaton <statonse@gmail.com>
2014-01-28 04:47:49 -0500

    make immutable and remove no moose syntax

--- lib/Graph/GEXF/Edge.pm
+++ lib/Graph/GEXF/Edge.pm
@@ -2,10 +2,10 @@ package Graph::GEXF::Edge;
 
 use Moose;
 use Data::UUID::LibUUID;
+use namespace::autoclean;
 
-with
-  'Graph::GEXF::Role::Viz::Size'  => { as  => 'thickness' },
-  'Graph::GEXF::Role::Viz::Shape' => { for => 'edge' };
+with 'Graph::GEXF::Role::Viz::Size'  => { as  => 'thickness' },
+     'Graph::GEXF::Role::Viz::Shape' => { for => 'edge' };
 
 has id => (
     is       => 'ro',
@@ -15,10 +15,13 @@ has id => (
 );
 
 has source => (is => 'ro', isa => 'Str', required => 1);
+
 has target => (is => 'ro', isa => 'Str', required => 1);
+
 has label  => (is => 'rw', isa => 'Str');
+
 has weight => (is => 'rw', isa => 'Num', lazy => 1, default => 1);
 
-no Moose;
+__PACKAGE__->meta->make_immutable;
 
 1;
