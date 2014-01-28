11d6b1e2549a9d60b0f610ac162b5cd500f8552b
sestaton <statonse@gmail.com>
2014-01-28 04:45:08 -0500

    remove the old no moose syntax

--- lib/Graph/GEXF/Attribute.pm
+++ lib/Graph/GEXF/Attribute.pm
@@ -1,16 +1,20 @@
 package Graph::GEXF::Attribute;
 
 use Moose;
+use namespace::autoclean;
 
 has id => (is => 'ro', isa => 'Int', required => 1,);
+
 has title => (is => 'rw', isa => 'Str');
+
 has value => (is => 'rw', isa => 'Str');
+
 has type  => (
     is  => 'ro',
     isa => enum([qw/string integer float double boolean date anyURI/])
 );
 
-no Moose;
+__PACKAGE__->meta->make_immutable;
 
 1;
 
